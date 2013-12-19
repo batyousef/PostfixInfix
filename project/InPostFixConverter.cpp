983821428579a9433f7016971f0aa4793cd8e18f
grubb2k7 <grubb2k7@vt.edu>
2013-12-19 01:26:12 -0500

    Final working version of main file...handles new appending and overwriting files

--- project/InPostFixConverter.cpp
+++ project/InPostFixConverter.cpp
@@ -12,19 +12,20 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	int opt;
-	bool option = false;
 	bool quiet = false;
 	bool overwrite = false;
+	bool append = false;
+	bool oa = false;
 
 	string line;
 	string temp;
+	string temp2;
 	char tempchar;
-	string overwritearg;
-	char* overwritechar;
+	string arg;
 	
 	opterr = 0;
 
-	while((opt = getopt(argc, argv, "hqo")) != -1)
+	while((opt = getopt(argc, argv, "hqoa")) != -1)
 	{
 		switch(opt) 
 		{
@@ -43,6 +44,11 @@ int main(int argc, char *argv[])
 				overwrite = true;
 				break;
 			}
+			case 'a':
+			{
+				append = true;
+				break;
+			}
 			default:
 			{
 				help();
@@ -50,64 +56,137 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
-	if(overwrite)
+	if(overwrite && append) oa = true;
+	if((optind + 1) == argc)
 	{
-		if((optind + 1) == argc)
+		if(oa)
 		{
-			cout << "\nType existing file name to be overwritten\n\n";
-		}
-		else
-		{
-			cout << "\nType 'original' to overwrite: " << argv[optind] << endl
-			<< "Type 'pass' to skip overwriting file: " << argv[optind] << endl
-			<< "Or\n"
-			<< "Type existing file name\n\n";
-		}
-		while(cin >> overwritearg)
-		{
-			temp = overwritearg;
-			transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
-			if(temp == "original" || temp == "pass")
+			cout << "Do you want to overwrite or append output to a file?\n"
+				<< "Type 'overwrite' or 'append'\n\n";
+			while(cin >> temp)
 			{
-				overwritearg = temp;
-				break;
-			}
-			ifstream in(overwritearg.c_str());
-			if(!in.is_open())
-			{
-				if((optind + 1) == argc)
+				transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+				if(temp == "overwrite")
 				{
-					cout << "\nCould not find file: " << overwritearg
-						<< "\nType existing file name to be overwritten\n\n";
+					overwrite = true;
+					append = false;
+					break;
+				}
+				else if(temp == "append")
+				{
+					overwrite = false;
+					append = true;
+					break;
 				}
 				else
 				{
-					cout << "\nCould not find file: "
-					<< overwritearg << ".\nPlease insert existing file "
-					<< "name\n"
-					<< "Type 'original' to overwrite: " << argv[optind] << endl
-					<< "Or\n"
-					<< "Type 'pass' to skip overwriting file: " << argv[optind] << endl << endl;
+					cout << "Do you want to overwrite or append output to a file?\n"
+						<< "Type 'overwrite' or 'append'\n\n";
 				}
 			}
-			else break;
 		}
-	}
-	if ((optind + 1) == argc)
-        {
+		if(append || overwrite)
+		{
+			if(overwrite)
+			{
+				cout << "\nType existing file name to be overwritten\n";
+			}
+			else if(append)
+			{
+				cout << "\nType existing file name to be appended\n";
+			}
+			cout << "OR\n" << "Type a new file name to create\n\n";
+			while(cin >> arg)
+			{
+				ifstream in(arg.c_str());
+				if(!in.is_open())
+				{
+					cout << "\nCould not find file: " << arg << endl
+							<< "Would you like to create a new file? (Y or N)\n\n";
+					cin >> temp;
+					transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+					while(true)
+					{
+						if(temp == "y")
+						{						
+							break;
+						}
+						else if (temp == "n")
+						{
+							cout << "\nType existing file name\n\n";
+							cin >> arg;
+							ifstream in(arg.c_str());
+							if(in.is_open())
+							{
+								break;
+							}
+							else
+							{
+								cout << "\nCould not find file: " << arg << endl
+									<< "Would you like to create a new file? (Y or N)\n\n";
+								cin >> temp;
+								transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+							}
+						}
+						else
+						{
+							cout << "Would you like to create a new file? (Y or N)\n\n";
+							cin >> temp;
+							transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+						}
+		
+					}
+					break;
+				}
+				else break;
+			}
+		}
 		cout << "\nType expression then press 'Enter, Ctrl+D'\n\n";
 		while(cin >> temp)
 		{
 			line.append(temp);
 		}
 		InPostFix C(line, *argv[argc-1]);
+		ofstream out;
+		if(append || overwrite)
+		{
+			if(overwrite)
+			{
+				out.open(arg.c_str(), ofstream::out | ofstream::trunc);
+			}
+			else if(append)
+			{
+				out.open(arg.c_str(), ofstream::out | ofstream::app);
+			}		
+			out << "\n\nOriginal Form:\n\n";
+			for(int i = 0; i < line.size(); i++)
+			{
+				if((i+1) == line.size() && line[i] != '=')
+				{
+					out << line[i] << " =\n";
+				}
+				else if(line[i] == '=')
+				{
+					out << " =\n";
+				}
+				else
+				{
+					out << line[i];
+				}
+			}
+			out.close();
+		}
 		line = C.GetExpression();
 		if(!quiet)
 		{
 			cout << endl;
 			if(overwrite)
 			{
-				cout << "Overwriting to: " << overwritearg << endl;
+				cout << "Overwriting to: " << arg << endl;
+			}
+			else if(append)
+			{
+				cout << "Appending to: " << arg << endl;
 			}
 			else
 			{
@@ -115,123 +194,180 @@ int main(int argc, char *argv[])
 			}
 			cout << line << endl << endl;
 		}
-		if(overwrite)
+		if(overwrite || append)
 		{
-			ofstream out;
-			out.open(overwritearg.c_str(), ofstream::out | ofstream::trunc);
-			out << line << endl;
+			out.open(arg.c_str(), ofstream::out | ofstream::app);
+			out << "\n\nConverted Form:\n\n" << line;
 			out.close();
 		}
+
 		return 0;
-        }
-        for (int i = optind; i < argc; i+=2)
+	}
+	else
+	{
+		for (int i = optind; i < argc; i+=2)
 
-        {
-
-                ifstream in(argv[i]);
-
-                if (!in.is_open())
-
-                {
-                        cerr <<  argv[i] << ": cannot be opened\n";
-			return 1;
-                }
-
-                else
-                {			
-			while (in >> tempchar)
+		{
+			if(oa)
 			{
-				temp = tempchar;
-				line.append(temp);			
-                	}
-			in.close();
-			InPostFix C(line, *argv[i+1]);
-			line = C.GetExpression();
-
-			if(!quiet)
+				cout << "Do you want to overwrite or append output for file: " << argv[i] << "?\n"
+					<< "Type 'overwrite' or 'append'\n\n";
+				while(cin >> temp)
+				{
+					transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+					if(temp == "overwrite")
+					{
+						overwrite = true;
+						append = false;
+						break;
+					}
+					else if(temp == "append")
+					{
+						overwrite = false;
+						append = true;
+						break;
+					}
+					else
+					{
+						cout << "Do you want to overwrite or append output to a file?\n"
+							<< "Type 'overwrite' or 'append'\n\n";
+					}
+				}
+			}
+			if(overwrite || append)
 			{
 				if(overwrite)
 				{
-					if(overwritearg == "original")
-					{
-						cout << "Overwritten to " << argv[i] << ":\n";
-					}
-					else if(overwritearg == "pass")
-					{
-						cout << "Overwritting did not occur.\n";
-					}
-					else
-					{
-						cout << "Overwritten to " << overwritearg << ":\n";
-					}				
+					cout << "\nType existing file name to be overwritten for file: " << argv[i] << endl;
 				}
-				cout << line << endl << endl;
-			}
-			if(overwrite)
-			{
-				if(overwritearg == "original")
+				else if(append)
 				{
-					ofstream out;
-					out.open(argv[i], ofstream::out | ofstream::trunc);
-					out << line << endl;
-					out.close();
+					cout << "\nType existing file name to be appended for file: " << argv[i] << endl;
 				}
-				else if(overwritearg == "pass")
+				cout << "OR\n" << "Type a new file name to create\n\n";
+				while(cin >> arg)
 				{
-				}
-				else
-				{
-					ifstream in(overwritearg.c_str());
+					ifstream in(arg.c_str());
 					if(!in.is_open())
 					{
-						cerr << "File " << overwritearg << " cannot be opened\n"
-							<< "Cannot overwrite file\n";
-						return 1;
+						cout << "\nCould not find file: " << arg << endl
+								<< "Would you like to create a new file? (Y or N)\n\n";
+						cin >> temp;
+						transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+						while(true)
+						{
+							if(temp == "y")
+							{						
+								break;
+							}
+							else if (temp == "n")
+							{
+								cout << "\nType existing file name\n\n";
+								cin >> arg;
+								ifstream in(arg.c_str());
+								if(in.is_open())
+								{
+									break;
+								}
+								else
+								{
+									cout << "\nCould not find file: " << arg << endl
+										<< "Would you like to create a new file? "
+										<< "(Y or N)\n\n";
+									cin >> temp;
+									transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+								}
+							}
+							else
+							{
+								cout << "Would you like to create a new file? (Y or N)\n\n";
+								cin >> temp;
+								transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+							}
+		
+						}
+						break;
+					}
+					else break;
+				}
+			}
+		        ifstream in(argv[i]);
+
+		        if (!in.is_open())
+
+		        {
+		                cerr <<  argv[i] << ": cannot be opened\n";
+				return 1;
+		        }
+
+		        else
+		        {			
+				while (in >> temp)
+				{
+					line.append(temp);			
+		        	}
+				ofstream out;
+				if(append || overwrite)
+				{
+					if(overwrite)
+					{
+						out.open(arg.c_str(), ofstream::out | ofstream::trunc);
+					}
+					else if(append)
+					{
+						out.open(arg.c_str(), ofstream::out | ofstream::app);
+					}		
+					out << "\n\nOriginal Form:\n\n";
+					for(int i = 0; i < line.size(); i++)
+					{
+						if((i+1) == line.size() && line[i] != '=')
+						{
+							out << line[i] << " =\n";
+						}
+						else if(line[i] == '=')
+						{
+							out << " =\n";
+						}
+						else
+						{
+							out << line[i];
+						}
+					}
+					out.close();
+				}
+				in.close();
+				InPostFix C(line, *argv[i+1]);
+				line = C.GetExpression();
+
+				if(!quiet)
+				{
+					cout << endl;
+					if(overwrite)
+					{
+						cout << "Overwriting to: " << arg << endl;
+					}
+					else if(append)
+					{
+						cout << "Appending to: " << arg << endl;
 					}
 					else
 					{
-						ofstream out;
-						out.open(overwritearg.c_str(), ofstream::out | ofstream::trunc);
-						out << line << endl;
-						out.close();
+						cout << "The converted expression is:\n";
 					}
+					cout << line << endl << endl;
 				}
-				if((i+2) != argc)
+				if(overwrite || append)
 				{
-					cout << "Type 'original' to overwrite the original file: " << argv[i+2] << endl
-						<< "Type 'pass' to skip writing data for file: " << argv[i+2] << endl
-						<< "Or\n"						
-						<< "Type an existing file name to be overwritten.\n\n";
-					while(cin >> overwritearg)
-					{
-						cout << endl;
-						temp = overwritearg;
-						transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
-						if(temp == "original" || temp == "pass")
-						{
-							overwritearg = temp;
-							break;
-						}
-						ifstream in(overwritearg.c_str());
-						if(!in.is_open())
-						{
-							cout << "\nCould not find file: " << overwritearg
-								<< ".\nPlease insert existing file "
-								<< "name\n"
-								<< "Type 'original' to overwrite: " << argv[optind] << endl
-								<< "Or\n"
-								<< "Type 'pass' to skip overwriting file: " << argv[optind] << endl
-								<< endl;
-						}
-						else break;
-					}
+					out.open(arg.c_str(), ofstream::out | ofstream::app);
+					out << "\n\nConverted Form:\n\n" << line;
+					out.close();
 				}
 			}
+			line.clear();
 		}
-		line.clear();
-        }
+	}
 
 
 
-return 0;
+	return 0;
 }
